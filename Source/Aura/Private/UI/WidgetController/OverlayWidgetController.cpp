ffce3aa580ecc22a8953502d26a9b563fb9f9773
Edwin Villanueva <edwincv0@gmail.com>
2024-07-03 10:00:36 -0700

    Work on Broadcast Delegate

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -2,4 +2,9 @@
 
 
 #include "UI/WidgetController/OverlayWidgetController.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	UAuraAttributeSet* AuraAttributeSet = CastChecked
+}
