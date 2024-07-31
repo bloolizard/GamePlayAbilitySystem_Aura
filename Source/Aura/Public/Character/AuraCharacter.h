ac6fee98fd6b7628d7d145c6b3a9fef9d7e80337
Edwin Villanueva <edwincv0@gmail.com>
2024-07-31 15:00:12 -0700

    Add Gameplay Tags + Debug Message for Gameplay Ability System.

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -19,6 +19,7 @@ public:
 
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
+
 private:
-	void InitAbilityActorInfo();
+	virtual void InitAbilityActorInfo() override;
 };
